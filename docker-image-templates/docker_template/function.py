67182bd3e1f4d23474d2e98ed0f3936146593008
Shreshth Tuli <shreshthtuli@gmail.com>
2021-04-30 16:29:26 +0100

    Update function.py

--- docker-image-templates/docker_template/function.py
+++ docker-image-templates/docker_template/function.py
@@ -6,4 +6,5 @@ def function(input_file_path, output_file_path):
     with Image.open(input_file_path) as image:
         ratio = max(image.size) / float(args)
         image.thumbnail(tuple(int(x / ratio) for x in image.size))
+        image = image.rotate(angle=90)
         image.save(output_file_path)
